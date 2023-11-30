b60950db62440bb1602b14868a8a0cf2b1b229e9
damin <34189426+damil27@users.noreply.github.com>
2023-11-30 21:04:27 +0100

    tailwind integration

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section>
+        <div>
+          <h1 className="text-4xl text-blue-400 font-bold pb-3  underline">Heading one</h1>
+          <p>
+            Explore the versatility of Tailwind CSS as you delve into the
+            intricacies of the Flex and Grid systems. Unleash the power of
+            flexible layouts with the intuitive Flex utility, allowing seamless
+            adaptation to diverse screen sizes. Simultaneously, harness the
+            potential of the Grid system to effortlessly structure complex
+            layouts. Mastering these features empowers you to create visually
+            stunning and responsive designs that captivate users across a
+            spectrum of devices.
+          </p>
+        </div>
+        <div>
+          <h1 className="text-4xl text-blue-400 font-bold pb-3 underline">Heading two</h1>
+          <p>
+            Explore the versatility of Tailwind CSS as you delve into the
+            intricacies of the Flex and Grid systems. Unleash the power of
+            flexible layouts with the intuitive Flex utility, allowing seamless
+            adaptation to diverse screen sizes. Simultaneously, harness the
+            potential of the Grid system to effortlessly structure complex
+            layouts. Mastering these features empowers you to create visually
+            stunning and responsive designs that captivate users across a
+            spectrum of devices.
+          </p>
+        </div>
+        <div>
+          <h1 className="text-4xl text-blue-400 font-bold pb-3 underline">Heading Three</h1>
+          <p>
+            Explore the versatility of Tailwind CSS as you delve into the
+            intricacies of the Flex and Grid systems. Unleash the power of
+            flexible layouts with the intuitive Flex utility, allowing seamless
+            adaptation to diverse screen sizes. Simultaneously, harness the
+            potential of the Grid system to effortlessly structure complex
+            layouts. Mastering these features empowers you to create visually
+            stunning and responsive designs that captivate users across a
+            spectrum of devices.
+          </p>
+        </div>
+        <div>
+          <h1 className="text-4xl text-blue-400 font-bold pb-3 underline">Heading Four</h1>
+          <p>
+            Explore the versatility of Tailwind CSS as you delve into the
+            intricacies of the Flex and Grid systems. Unleash the power of
+            flexible layouts with the intuitive Flex utility, allowing seamless
+            adaptation to diverse screen sizes. Simultaneously, harness the
+            potential of the Grid system to effortlessly structure complex
+            layouts. Mastering these features empowers you to create visually
+            stunning and responsive designs that captivate users across a
+            spectrum of devices.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App
