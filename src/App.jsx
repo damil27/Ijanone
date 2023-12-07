a39f0c2efdb06681e8e7020ebce2385862b701f4
damin <34189426+damil27@users.noreply.github.com>
2023-12-07 20:59:27 +0100

    create navigation and hero banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+import bg from "./assets/images/BG-1.png";
+import play from "./assets/images/play.png"
 
 function App() {
-
   return (
     <>
-      <section>
-        <div>
-          <h1 className="text-4xl text-blue-400 font-bold pb-3  underline">Heading one</h1>
-          <p>
-            Explore the versatility of Tailwind CSS as you delve into the
-            intricacies of the Flex and Grid systems. Unleash the power of
-            flexible layouts with the intuitive Flex utility, allowing seamless
-            adaptation to diverse screen sizes. Simultaneously, harness the
-            potential of the Grid system to effortlessly structure complex
-            layouts. Mastering these features empowers you to create visually
-            stunning and responsive designs that captivate users across a
-            spectrum of devices.
-          </p>
-        </div>
-        <div>
-          <h1 className="text-4xl text-blue-400 font-bold pb-3 underline">Heading two</h1>
-          <p>
-            Explore the versatility of Tailwind CSS as you delve into the
-            intricacies of the Flex and Grid systems. Unleash the power of
-            flexible layouts with the intuitive Flex utility, allowing seamless
-            adaptation to diverse screen sizes. Simultaneously, harness the
-            potential of the Grid system to effortlessly structure complex
-            layouts. Mastering these features empowers you to create visually
-            stunning and responsive designs that captivate users across a
-            spectrum of devices.
-          </p>
-        </div>
-        <div>
-          <h1 className="text-4xl text-blue-400 font-bold pb-3 underline">Heading Three</h1>
-          <p>
-            Explore the versatility of Tailwind CSS as you delve into the
-            intricacies of the Flex and Grid systems. Unleash the power of
-            flexible layouts with the intuitive Flex utility, allowing seamless
-            adaptation to diverse screen sizes. Simultaneously, harness the
-            potential of the Grid system to effortlessly structure complex
-            layouts. Mastering these features empowers you to create visually
-            stunning and responsive designs that captivate users across a
-            spectrum of devices.
-          </p>
-        </div>
-        <div>
-          <h1 className="text-4xl text-blue-400 font-bold pb-3 underline">Heading Four</h1>
-          <p>
-            Explore the versatility of Tailwind CSS as you delve into the
-            intricacies of the Flex and Grid systems. Unleash the power of
-            flexible layouts with the intuitive Flex utility, allowing seamless
-            adaptation to diverse screen sizes. Simultaneously, harness the
-            potential of the Grid system to effortlessly structure complex
-            layouts. Mastering these features empowers you to create visually
-            stunning and responsive designs that captivate users across a
-            spectrum of devices.
-          </p>
-        </div>
-      </section>
+      <div>
+        <nav className="container mx-auto py-6  flex justify-between items-center ">
+          <div>
+            {/* logo  */}
+            foltFoundation i
+          </div>
+          <div>
+            {/* link  */}
+            <ul className="flex justify-center gap-5">
+              <li>Home</li>
+              <li>What we do</li>
+              <li>Media</li>
+              <li>About Us</li>
+            </ul>
+          </div>
+          <div>
+            {/* button  */}
+            <button className="bg-black text-white py-3 px-6 rounded-lg font-bold ">
+              Donate
+            </button>
+          </div>
+        </nav>
+        {/* hero banner start here  */}
+        <section
+          className="h-[32rem]"
+          style={{
+            background: `url(${bg})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <div className="container mx-auto relative ">
+            <div className=" absolute top-[5rem]">
+              <div>
+                {/* header */}
+                <h1 className="text-white text-4xl font-bold ">
+                  Inclusive care for <br /> children with special <br /> needs
+                </h1>
+              </div>
+              <div className="mt-[3.5rem] flex items-center justify-start ">
+                {/* button and play  */}
+                <button className="bg-white py-2  px-4 rounded mr-3 ">
+                  what we do
+                </button>
+                <div className="text-white flex gap-4 ">
+                  <img src={play} alt="" />
+                  <p>
+
+                  play video
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div></div>
+          </div>
+        </section>
+      </div>
     </>
   );
 }
 
-export default App
+export default App;
